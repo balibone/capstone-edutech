54f95499dee23252aa23a6e959244d1e216e4338
Derian Tungka <balibone@gmail.com>
2018-01-30 08:44:49 +0800

    Exposed user entity as a web service

---
+++ EduTech/EduTechWebApp-war/src/java/entities/UserEntity.java
@@ -0,0 +1,41 @@
+package entities;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity(name = "SystemUser")
+public class UserEntity implements Serializable {
+    @Id
+    private String userEmail;
+    private String userPassword;
+    private String userType;
+    private Boolean userActiveStatus;
+    
+    @Temporal(TemporalType.DATE)
+    private Date userCreationDate;
+    
+    @PrePersist
+    public void creationDate() { this.userCreationDate = new Date(); }
+    
+    /* DEFAULT CONSTRUCTOR */
+    public UserEntity() { userActiveStatus = true; }
+    
+    /* GETTER METHODS */
+    public String getUserEmail() { return userEmail; }
+    public String getUserPassword() { return userPassword; }
+    public String getUserType() { return userType; }
+    public Boolean getUserActiveStatus() { return userActiveStatus; }
+    public Date getUserCreationDate() { return userCreationDate; }
+    
+    /* SETTER METHODS */
+    public void setUserEmail(String userEmail) { this.userEmail = userEmail; }
+    public void setUserPassword(String userPassword) { this.userPassword = userPassword; }
+    public void setUserType(String userType) { this.userType = userType; }
+    public void setUserActiveStatus(Boolean userActiveStatus) { this.userActiveStatus = userActiveStatus; }
+    public void setUserCreationDate(Date userCreationDate) { this.userCreationDate = userCreationDate; }
+}
