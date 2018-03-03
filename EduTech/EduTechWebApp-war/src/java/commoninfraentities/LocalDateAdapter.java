7d5b3be6769c384436bd8cfce1a2ad599f285591
Derian Tungka <balibone@gmail.com>
2018-03-03 15:28:43 +0800

    Added XML Adapters for java.time attributes

---
+++ EduTech/EduTechWebApp-war/src/java/commoninfraentities/LocalDateAdapter.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package commoninfraentities;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import javax.xml.bind.annotation.adapters.XmlAdapter;
+
+/**
+ *
+ * @author Derian
+ */
+public class LocalDateAdapter extends XmlAdapter<String, LocalDate> {
+
+    @Override
+    public LocalDate unmarshal(String v) throws Exception {
+        return LocalDate.parse(v);
+    }
+
+    @Override
+    public String marshal(LocalDate v) throws Exception {
+        return v.format(DateTimeFormatter.ISO_LOCAL_DATE);
+    }
+    
+}
