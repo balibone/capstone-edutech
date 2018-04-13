e72b1b46406579e19d16251acc1682f626bcc0fb
N <nigelee@gmail.com>
2018-04-13 12:37:37 +0800

    New event

--- EduTech/EduTech-ejb/src/java/unifyentities/event/EventEntity.java
+++ EduTech/EduTech-ejb/src/java/unifyentities/event/EventEntity.java
@@ -23,6 +23,12 @@ import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
 import commoninfrastructureentities.UserEntity;
+import unifyentities.common.EventRequestEntity;
+import java.util.ArrayList;
+import java.util.Collection;
+import javax.persistence.CascadeType;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 
 @Entity(name = "Event")
 public class EventEntity implements Serializable {
@@ -31,6 +37,7 @@ public class EventEntity implements Serializable {
     private Long eventID;
     private String eventStatus;
     private String eventDescription;
+    private String eventTitle;
     @Temporal(TemporalType.TIMESTAMP)
     private Date eventStartDateTime;
     @Temporal(TemporalType.TIMESTAMP)
@@ -43,18 +50,28 @@ public class EventEntity implements Serializable {
     @ManyToOne
     private UserEntity userEntity;
     
+    @OneToMany(cascade = {CascadeType.REMOVE}, mappedBy = "eventEntity")
+    private Collection<EventRSVPEntity> eventRsvpSet = new ArrayList<EventRSVPEntity>();
+    @OneToMany(cascade = {CascadeType.REMOVE}, mappedBy = "eventEntity")
+    private Collection<EventReportEntity> eventReportSet = new ArrayList<EventReportEntity>();
+    
+    @OneToOne(cascade = {CascadeType.REMOVE})
+    private EventRequestEntity eventRequestEntity;
+    
     @PrePersist
     public void creationDate() { this.eventApprovalDate = new Date(); }
     
     /* MISCELLANEOUS METHODS */
-    public void createEvent(String eventDescription, String eventVenue, 
+    public void createEvent(String eventTitle, String eventDescription, String eventVenue, 
             Date eventStartDateTime, Date eventEndDateTime, UserEntity eventRequestor) {
+        this.eventTitle = eventTitle;
         this.eventDescription = eventDescription;
         this.eventVenue = eventVenue;
         this.eventStartDateTime = eventStartDateTime;
         this.eventEndDateTime = eventEndDateTime;
         this.eventStatus = "Active";
         this.userEntity = eventRequestor;
+        //add event title
     }
     
     /* GETTER METHODS */
@@ -65,8 +82,13 @@ public class EventEntity implements Serializable {
     public Date getEventStartDateTime() { return eventStartDateTime; }
     public Date getEventEndDateTime() { return eventEndDateTime; }
     public String getEventVenue() { return eventVenue; }
+    public String getEventTitle() { return eventTitle; }
     
     public UserEntity getUserEntity() { return userEntity; }
+    public EventRequestEntity getEventRequestEntity() { return eventRequestEntity; }
+    
+    public Collection<EventRSVPEntity> getEventRsvpSet() { return eventRsvpSet; }
+    public Collection<EventReportEntity> getEventReportSet() { return eventReportSet; }
     
     /* SETTER METHODS */
     public void setEventID(Long eventID) { this.eventID = eventID; }
@@ -76,6 +98,8 @@ public class EventEntity implements Serializable {
     public void setEventStartDateTime(Date eventStartDateTime) { this.eventStartDateTime = eventStartDateTime; }
     public void setEventEndDateTime(Date eventEndDateTime) { this.eventEndDateTime = eventEndDateTime; }
     public void setEventVenue(String eventVenue) {this.eventVenue = eventVenue; }
+    public void setEventTitle(String eventTitle) {this.eventTitle = eventTitle; }
     
     public void setUserEntity(UserEntity userEntity) { this.userEntity = userEntity; }
+    public void setEventRequestEntity(EventRequestEntity eventRequestEntity) { this.eventRequestEntity = eventRequestEntity; }
 }
