7a572f3405ea21bb0ed1a5461adc389502d14ff1
N <nigelee@gmail.com>
2018-04-13 12:39:46 +0800

    View all events listing

---
+++ EduTech/EduTechWebApp-war/web/js/unify/systemuser/webjs/events/ViewEventDetailsSYSJS.js
@@ -0,0 +1,64 @@
+function rsvpEvent(eventID) {
+
+    if (confirm('Confirm RSVP to event?')) {
+
+        $.get('EventsSysUser?pageTransit=goToRsvpToEventSYS&hiddenEventID=' + eventID + '');
+
+        $('#rsvpEvent-alert').iziModal('open', event);
+    }
+}
+
+var center = L.bounds([1.56073, 104.11475], [1.16, 103.502]).getCenter();
+var map = L.map('mapdiv').setView([center.x, center.y], 12);
+var search_marker, dataString;
+
+var basemap = L.tileLayer('https://maps-{s}.onemap.sg/v3/Default/{z}/{x}/{y}.png', {
+    detectRetina: true,
+    maxZoom: 18,
+    minZoom: 11,
+    id: 'your.mapbox.project.id',
+    accessToken: 'your.mapbox.public.access.token'
+});
+
+$(document).ready(function () {
+    $('#unifyPageNAV').load('webapp/unify/systemuser/masterpage/PageNavigation.jsp');
+    $('#unifyFooter').load('webapp/unify/systemuser/masterpage/PageFooter.jsp');
+
+    var tradeLocation = document.getElementById("venueLocation").value;
+    var tradeLatitude = document.getElementById("venueLat").value;
+    var tradeLongitude = document.getElementById("venueLong").value;
+
+    map.setMaxBounds([[1.56073, 104.1147], [1.16, 103.502]]);
+    basemap.addTo(map);
+
+    var pointerIcon = L.icon({
+        iconUrl: 'images/pointer.png',
+        iconSize: [38, 40],
+        iconAnchor: [22, 94],
+        popupAnchor: [-3, -76]
+    });
+
+    if (search_marker) { map.removeLayer(search_marker); }
+    search_marker = L.marker([tradeLatitude, tradeLongitude], {draggable: false, icon: pointerIcon}).addTo(map).bindPopup("Trade Location: " + tradeLocation);        
+    
+    $("a[href='#venuePane']").on('shown.bs.tab', function(e) {
+      map.invalidateSize();
+ });
+ 
+ $('#rsvpEvent-alert').iziModal({
+        title: 'RSVPed!',
+        iconClass: 'fa fa-bookmark',
+        transitionIn: 'transitionIn',
+        transitionOut: 'transitionOut',
+        width: 400,
+        overlayClose: true,
+        timeout: 10,
+        timeoutProgressbar: true,
+        pauseOnHover: false,
+        timeoutProgressbarColor: 'rgba(255,255,255,0.5)',
+        onClosed: function () {
+            window.location.reload(true);
+        }
+    });
+ 
+});
---
+++ EduTech/EduTechWebApp-war/web/js/unify/systemuser/webjs/events/ViewEventsListingSYSJS.js
@@ -0,0 +1,379 @@
+function viewComment(shoutIDContent) {
+
+    //alert(shoutIDContent)
+
+    if (!shoutIDContent)
+        return [];
+    var i = shoutIDContent.indexOf(',');
+    if (i > 0) {
+
+        //alert(shoutIDContent.substring(0, i));
+        //alert(shoutIDContent.substring(i + 1));
+
+        $('iframe').attr('src', 'ShoutsSysUser?pageTransit=goToViewShoutCommentModalSYS&hiddenShoutID=' + shoutIDContent.substring(0, i) + '&hiddentShoutContent=' + shoutIDContent.substring(i + 1) + '');
+    } else
+        return [shoutIDContent];
+
+    //$('iframe').attr('src', 'ShoutsSysUser?pageTransit=goToViewShoutCommentModalSYS&hiddenShoutID=' + shoutID + '');
+    $('#viewComment-iframe').iziModal('open', event);
+}
+
+function eventDetails(eventID) {
+
+        //alert('test');
+        //$().attr('src', 'EventsSysUser?pageTransit=goToViewEventSYS&hiddenEventID=' + eventID + '');
+        
+        //$(this).attr('target', 'EventsSysUser?pageTransit=goToViewEventSYS&hiddenEventID=' + eventID + '');
+   
+        window.location.href = 'EventsSysUser?pageTransit=goToViewEventDetailsSYS&hiddenEventID=' + eventID + '';
+}
+
+function reportShout(shoutIDContent) {
+
+    if (!shoutIDContent)
+        return [];
+    var i = shoutIDContent.indexOf(',');
+    if (i > 0) {
+        //$('iframe').attr('src', 'ShoutsSysUser?pageTransit=goToViewShoutCommentModalSYS&hiddenShoutID=' + shoutIDContent.substring(0, i) + '&hiddentShoutContent=' + shoutIDContent.substring(i + 1) + '');
+    } else
+        return [shoutIDContent];
+
+
+    $('iframe').attr('src', 'ShoutsSysUser?pageTransit=goToReportShoutModalSYS&hiddenShoutID=' + shoutIDContent.substring(0, i) + '&hiddentShoutContent=' + shoutIDContent.substring(i + 1) + '');
+    $('#reportShout-iframe').iziModal('open', event);
+}
+
+function deleteAlert(shoutID) {
+
+    if (confirm('Sure you want to delete your shout?')) {
+
+        $.get('ShoutsSysUser?pageTransit=goToDeleteShoutSYS&hiddenShoutID=' + shoutID + '');
+
+        $('#deleteShout-alert').iziModal('open', event);
+    }
+}
+
+function bookmarkAlert(shoutIDusername) {
+
+    //alert(shoutIDusername)
+
+    if (!shoutIDusername)
+        return [];
+    var i = shoutIDusername.indexOf(',');
+    if (i > 0) {
+        //alert("TEST");
+        //alert(shoutIDusername.substring(0, i));
+        //alert(shoutIDusername.substring(i + 1));
+        $.get('ShoutsSysUser?pageTransit=goToSetBookmarkSYS&hiddenShoutID=' + shoutIDusername.substring(0, i) + '&loggedInUsername=' + shoutIDusername.substring(i + 1) + '');
+        //return [shoutIDusername.substring(0, i), shoutIDusername.substring(i + 1)];
+
+    } else
+        return [shoutIDusername];
+
+    $('#bookmark-alert').iziModal('open', event);
+}
+
+function unbookmarkAlert(shoutIDusername) {
+
+    //alert(shoutIDusername)
+
+    if (!shoutIDusername)
+        return [];
+    var i = shoutIDusername.indexOf(',');
+    if (i > 0) {
+        //alert("TEST");
+        //alert(shoutIDusername.substring(0, i));
+        //alert(shoutIDusername.substring(i + 1));
+        $.get('ShoutsSysUser?pageTransit=goToRemoveBookmarkSYS&hiddenShoutID=' + shoutIDusername.substring(0, i) + '&loggedInUsername=' + shoutIDusername.substring(i + 1) + '');
+        //return [shoutIDusername.substring(0, i), shoutIDusername.substring(i + 1)];
+
+    } else
+        return [shoutIDusername];
+
+    $('#unbookmark-alert').iziModal('open', event);
+}
+
+function bookmarkFrame(shoutIDusername) {
+
+    alert(shoutIDusername)
+
+    if (!shoutIDusername)
+        return [];
+    var i = shoutIDusername.indexOf(',');
+    if (i > 0) {
+        alert("TEST");
+        alert(shoutIDusername.substring(0, i));
+        alert(shoutIDusername.substring(i + 1));
+        $('iframe').attr('src', 'ShoutsSysUser?pageTransit=goToSetBookmarkSYS&hiddenShoutID=' + shoutIDusername.substring(0, i) + '&loggedInUsername=' + shoutIDusername.substring(i + 1) + '');
+        //return [shoutIDusername.substring(0, i), shoutIDusername.substring(i + 1)];
+
+    } else
+        return [shoutIDusername];
+
+
+    $('#bookmark-iframe').iziModal('open', event);
+}
+
+function likeAlert(shoutIDusername) {
+
+    //alert(shoutIDusername)
+
+    if (!shoutIDusername)
+        return [];
+    var i = shoutIDusername.indexOf(',');
+    if (i > 0) {
+        //alert("likeShout TEST");
+        //alert(shoutIDusername.substring(0, i));
+        //alert(shoutIDusername.substring(i + 1));
+        //$.get('localhost/myWebbApp/executeInit.jsp');
+        //return [shoutIDusername.substring(0, i), shoutIDusername.substring(i + 1)];
+        $.get('ShoutsSysUser?pageTransit=goToSetLikeSYS&hiddenShoutID=' + shoutIDusername.substring(0, i) + '&loggedInUsername=' + shoutIDusername.substring(i + 1) + '');
+
+    } else
+        return [shoutIDusername];
+
+
+    $('#like-alert').iziModal('open', event);
+}
+
+function unlikeAlert(shoutIDusername) {
+
+    //alert(shoutIDusername)
+
+    if (!shoutIDusername)
+        return [];
+    var i = shoutIDusername.indexOf(',');
+    if (i > 0) {
+        //alert("likeShout TEST");
+        //alert(shoutIDusername.substring(0, i));
+        //alert(shoutIDusername.substring(i + 1));
+        //$.get('localhost/myWebbApp/executeInit.jsp');
+        //return [shoutIDusername.substring(0, i), shoutIDusername.substring(i + 1)];
+        $.get('ShoutsSysUser?pageTransit=goToRemoveLikeSYS&hiddenShoutID=' + shoutIDusername.substring(0, i) + '&loggedInUsername=' + shoutIDusername.substring(i + 1) + '');
+
+    } else
+        return [shoutIDusername];
+
+
+    $('#unlike-alert').iziModal('open', event);
+}
+
+function newShout() {
+    $('iframe').attr('src', 'ShoutsSysUser?pageTransit=goToNewShoutModalSYS');
+    $('#newShout-iframe').iziModal('open', event);
+}
+
+function newEventRequest() {
+    $('iframe').attr('src', 'EventsSysUser?pageTransit=goToNewEventModalSYS');
+    $('#newEventRequest-iframe').iziModal('open', event);
+}
+
+$(document).ready(function () {
+    $('#unifyPageNAV').load('webapp/unify/systemuser/masterpage/PageNavigation.jsp');
+    $('#unifyFooter').load('webapp/unify/systemuser/masterpage/PageFooter.jsp');
+
+    jQuery.fn.jplist.settings = {
+        priceSlider: function ($slider) {
+            $slider.slider({
+                min: 0, max: 1000, range: true, values: [0, 1000]
+                , slide: function (event, ui) {
+                    $('#min-price').val(ui.values[0]);
+                    $('#max-price').val(ui.values[1]);
+                }
+            });
+        },
+        priceValues: function ($slider) {
+            $('#min-price').val($slider.slider('values', 0));
+            $('#max-price').val($slider.slider('values', 1));
+        }
+    };
+    $('#contentArea').jplist({
+        itemsBox: '.list', itemPath: '.list-item', panelPath: '.jplist-search'
+    });
+
+    $('.card').on('click', function (event) {
+        alert(here);
+        var itemID = $(this).attr('id');
+        if (itemID !== null) {
+            $('iframe').attr('src', 'MarketplaceSysUser?pageTransit=goToViewItemDetailsModalSYS&itemID=' + itemID);
+            $('#eventCard-iframe').iziModal('open', event);
+        } else {
+            alert("Item cannot be found. Please refresh the page and try again.");
+        }
+    });
+
+    $('#viewEvent-iframe').iziModal({
+        title: 'Event Details',
+        iconClass: 'fa fa-comments',
+        transitionIn: 'transitionIn',
+        transitionOut: 'transitionOut',
+        headerColor: '#4D7496',
+        width: 500,
+        overlayClose: true,
+        iframe: true,
+        iframeHeight: 500,
+        onClosed: function () {
+            window.location.reload(true);
+        }
+    });
+
+
+    $('#viewComment-iframe').iziModal({
+        title: 'Comments',
+        iconClass: 'fa fa-comments',
+        transitionIn: 'transitionIn',
+        transitionOut: 'transitionOut',
+        headerColor: '#4D7496',
+        width: 500,
+        overlayClose: true,
+        iframe: true,
+        iframeHeight: 500,
+        onClosed: function () {
+            window.location.reload(true);
+        }
+    });
+    
+    $('#newEventRequest-iframe').iziModal({
+        title: 'Submit Event Request',
+        iconClass: 'fa fa-calendar-plus-o',
+        transitionIn: 'transitionIn',
+        transitionOut: 'transitionOut',
+        headerColor: '#4D7496',
+        width: 500,
+        overlayClose: true,
+        iframe: true,
+        iframeHeight: 500,
+        onClosed: function () {
+            window.location.reload(true);
+        }
+    });
+
+    $('#newShout-iframe').iziModal({
+        title: 'Create A Shout',
+        iconClass: 'fa fa-bullhorn',
+        transitionIn: 'transitionIn',
+        transitionOut: 'transitionOut',
+        headerColor: '#4D7496',
+        width: 500,
+        overlayClose: true,
+        iframe: true,
+        iframeHeight: 500,
+        onClosed: function () {
+            window.location.reload(true);
+        }
+    });
+
+    $('#reportShout-iframe').iziModal({
+        title: 'Report Shout',
+        iconClass: 'fa fa-flag',
+        transitionIn: 'transitionIn',
+        transitionOut: 'transitionOut',
+        headerColor: '#4D7496',
+        width: 500,
+        overlayClose: true,
+        iframe: true,
+        iframeHeight: 450,
+    });
+
+    $('#bookmark-iframe').iziModal({
+        title: 'Bookmark',
+        iconClass: 'fa fa-bookmark',
+        transitionIn: 'transitionIn',
+        transitionOut: 'transitionOut',
+        headerColor: '#4D7496',
+        width: 500,
+        overlayClose: true,
+        iframe: true,
+        iframeHeight: 100
+    });
+
+    $('#deleteShout-alert').iziModal({
+        title: 'Shout Deleted!',
+        iconClass: 'fa fa-trash',
+        transitionIn: 'transitionIn',
+        transitionOut: 'transitionOut',
+        width: 400,
+        overlayClose: true,
+        timeout: 10,
+        timeoutProgressbar: true,
+        pauseOnHover: false,
+        timeoutProgressbarColor: 'rgba(255,255,255,0.5)',
+        onClosed: function () {
+            window.location.reload(true);
+        }
+    });
+
+    $('#bookmark-alert').iziModal({
+        title: 'Shout Bookmarked!',
+        iconClass: 'fa fa-bookmark',
+        transitionIn: 'transitionIn',
+        transitionOut: 'transitionOut',
+        width: 400,
+        overlayClose: true,
+        timeout: 10,
+        timeoutProgressbar: true,
+        pauseOnHover: false,
+        timeoutProgressbarColor: 'rgba(255,255,255,0.5)',
+        onClosed: function () {
+            window.location.reload(true);
+        }
+    });
+
+    $('#unbookmark-alert').iziModal({
+        title: 'Shout Unbookmarked!',
+        iconClass: 'fa fa-bookmark',
+        transitionIn: 'transitionIn',
+        transitionOut: 'transitionOut',
+        width: 400,
+        overlayClose: true,
+        timeout: 10,
+        timeoutProgressbar: true,
+        pauseOnHover: false,
+        timeoutProgressbarColor: 'rgba(255,255,255,0.5)',
+        onClosed: function () {
+            window.location.reload(true);
+        }
+    });
+
+    $('#like-alert').iziModal({
+        title: 'Shout Liked!',
+        iconClass: 'fa fa-bookmark',
+        transitionIn: 'transitionIn',
+        transitionOut: 'transitionOut',
+        width: 400,
+        overlayClose: true,
+        timeout: 10,
+        timeoutProgressbar: true,
+        pauseOnHover: false,
+        timeoutProgressbarColor: 'rgba(255,255,255,0.5)',
+        onClosed: function () {
+            window.location.reload(true);
+        }
+    });
+
+
+
+    $('#unlike-alert').iziModal({
+        title: 'Shout Unliked!',
+        iconClass: 'fa fa-bookmark',
+        transitionIn: 'transitionIn',
+        transitionOut: 'transitionOut',
+        width: 400,
+        overlayClose: true,
+        timeout: 10,
+        timeoutProgressbar: true,
+        pauseOnHover: false,
+        timeoutProgressbarColor: 'rgba(255,255,255,0.5)',
+        onClosed: function () {
+            window.location.reload(true);
+        }
+    });
+
+
+    $('#closeSuccess').click(function () {
+        $('#successPanel').fadeOut(300);
+    });
+    $('#closeError').click(function () {
+        $('#errorPanel').fadeOut(300);
+    });
+});
