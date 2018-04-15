ec42c2c2903ca1d9ee5da86f08687308aabd24a2
Winston <xvesperviperx@gmail.com>
2018-04-15 09:03:23 +0800

    Partial-Fixes for Errands (Unify Admin)
    Partial-Fixes for Errands (Unify Admin)

--- EduTech/EduTechWebApp-war/web/js/unify/systemuser/webjs/marketplace/EditItemListingSYSJS.js
+++ EduTech/EduTechWebApp-war/web/js/unify/systemuser/webjs/marketplace/EditItemListingSYSJS.js
@@ -84,17 +84,11 @@ $(document).ready(function () {
     $('.owl-carousel').owlCarousel({
         loop: false,
         margin: 10,
-        nav:    true,
+        nav: true,
         responsive:{
-            0:{
-                items:1
-            },
-            600:{
-                items:3
-            },
-            1000:{
-                items:5
-            }
+            0: { items: 1 },
+            600: { items: 3 },
+            1000: { items: 5 }
         }
     });
 });
