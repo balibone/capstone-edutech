8c19c86079701c435c93b5640517ce1725d0246b
Derian Tungka <balibone@gmail.com>
2018-04-17 15:26:41 +0800

    Landing page & localhost fix
    1) Landing page update.
    2) Changed all localhost to be server address

--- EduTechRemoteInterface/src/commoninfrasessionbeans/CommonInfraMgrBeanRemote.java
+++ EduTechRemoteInterface/src/commoninfrasessionbeans/CommonInfraMgrBeanRemote.java
@@ -6,6 +6,7 @@ import javax.ejb.Remote;
 
 @Remote
 public interface CommonInfraMgrBeanRemote {
+    
     public boolean empLogin(String username, String password);
     public boolean createSysUser(String salutation, String firstName, String lastName, String username, String password, String email, String contactNum);
 
