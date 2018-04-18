41388e4030e944d50a84b7c80e8577b6a0db89ff
Winston <xvesperviperx@gmail.com>
2018-04-19 07:22:46 +0800

    Fixes for Unify Errands (Unify System)
    Fixes for Unify Errands (Unify System)

--- EduTech/EduTechWebApp-war/web/js/unify/systemuser/webjs/errands/NewJobListingSYSJS.js
+++ EduTech/EduTechWebApp-war/web/js/unify/systemuser/webjs/errands/NewJobListingSYSJS.js
@@ -146,7 +146,7 @@ var helperInput = document.getElementById('numOfHelpers');
 jobRateInput.onkeydown = function(e) {
     if(!((e.keyCode > 95 && e.keyCode < 106)
       || (e.keyCode > 47 && e.keyCode < 58) 
-      || e.keyCode == 8 || keyCode === 190)) {
+      || e.keyCode == 8 || e.keyCode === 190)) {
         return false;
     }
 };
